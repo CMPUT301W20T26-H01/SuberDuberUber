8734fa340db8ac05684bec610c37af0ec6b14321
Stephen Ta <sta1@ualberta.ca>
2020-02-25 14:45:14 -0700

    US 00.01: Added the User, Driver, and Rider superclasses.

---
+++ app/src/main/java/com/example/suberduberuber/Rider.java
@@ -0,0 +1,42 @@
+package com.example.suberduberuber;
+
+import java.util.ArrayList;
+
+public class Rider extends User {
+    private ArrayList<String> rides; // TODO: Change String to Ride when class is created
+    private ArrayList<String> requests; // TODO: Change String to Request when class is created
+
+    public Rider(String username, String phoneNumber, String emailAddress) {
+        super(username, phoneNumber, emailAddress);
+        this.rides = new ArrayList<String>();
+        this.requests = new ArrayList<String>();
+    }
+
+    public void addRide(String ride) {
+        this.rides.add(ride);
+    }
+
+    public void removeRide(String ride) {
+        if (this.rides.contains(ride)) {
+            this.rides.remove(ride);
+        }
+    }
+
+    public void addRequest(String request) {
+        this.requests.add(request);
+    }
+
+    public void removeRequest(String request) {
+        if (this.requests.contains(request)) {
+            this.requests.remove(request);
+        }
+    }
+
+    public ArrayList<String> getRides() {
+        return this.rides;
+    }
+
+    public ArrayList<String> getRequests() {
+        return this.requests;
+    }
+}
