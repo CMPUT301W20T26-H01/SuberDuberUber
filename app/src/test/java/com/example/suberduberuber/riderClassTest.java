9dca67e64c800f8c154dbb56d4ff3d64112fe29f
Stephen Ta <sta1@ualberta.ca>
2020-03-01 22:18:17 -0700

    Added rider class tests

--- app/src/test/java/com/example/suberduberuber/riderClassTest.java
+++ app/src/test/java/com/example/suberduberuber/riderClassTest.java
@@ -11,8 +11,49 @@ public class riderClassTest {
     }
 
     @Test
-    void testAddRide() {
+    public void testAddRide() {
         Rider r = createRider();
+        String ride = "Ride1";
+        r.addRide(ride);
+
+        assertEquals(ride, r.getRides().get(0));
+        r.addRide("Ride2");
+
+        assertEquals(2, r.getRides().size());
+        assertEquals("Ride2", r.getRides().get(1));
+    }
+
+    @Test
+    public void testRemoveRide() {
+        Rider r = createRider();
+        String ride = "Ride1";
+        r.addRide(ride);
+        r.removeRide(ride);
+
+        assertEquals(0, r.getRides().size());
+    }
+
+    @Test
+    public void testAddRequest() {
+        Rider r = createRider();
+        String req = "Request1";
+        r.addRequest(req);
+
+        assertEquals(req, r.getRequests().get(0));
+        r.addRequest("Request2");
+
+        assertEquals(2, r.getRequests().size());
+        assertEquals("Request2", r.getRequests().get(1));
+    }
+
+    @Test
+    public void testRemoveRequest() {
+        Rider r = createRider();
+        String req = "Request1";
+        r.addRequest(req);
+        r.removeRequest(req);
+
+        assertEquals(0, r.getRequests().size());
     }
 
 
