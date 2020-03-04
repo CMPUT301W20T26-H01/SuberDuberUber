05791d7f5ae637b4cc545c15546f957593335cc5
Stephen Ta <sta1@ualberta.ca>
2020-03-04 11:59:41 -0700

    Adding QR code generation

---
+++ app/src/main/java/com/example/suberduberuber/Fragments/QRCodeFragment.java
@@ -0,0 +1,93 @@
+package com.example.suberduberuber.Fragments;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.suberduberuber.R;
+import com.example.suberduberuber.Repositories.UserRepository;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+public class QRCodeFragment extends Fragment {
+    private NavController navController;
+    private UserRepository firestoreRepository;
+
+    private Button genQRCodeButton;
+    private Bitmap bitmap;
+    public final static int qrWidth = 500;
+
+
+    public QRCodeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_qrcode_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+
+        firestoreRepository = new UserRepository();
+
+        DocumentReference user = firestoreRepository.getCurrentUser();
+
+        genQRCodeButton = view.findViewById(R.id.qrGen_button);
+        genQRCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String id = user.getId();
+                try {
+                    bitmap = generateQRCode(id);
+                    // imageView.setImageBitmap(bitmap);
+                }
+                catch (WriterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public Bitmap generateQRCode(String id) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(id, BarcodeFormat.DATA_MATRIX.QR_CODE, qrWidth, qrWidth, null);
+        }
+        catch (IllegalArgumentException e){
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+        int bitMatrixHeight = bitMatrix.getHeight();
+        int [] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.QRCodeBlackColor): getResources().getColor(R.color.QRCodeWhiteColor);
+            }
+        }
+        Bitmap bp = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+        bp.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bp;
+    }
+}
