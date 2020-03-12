881ef1e55b3667b66aa3d0306934e8051364a2c4
Stephen Ta <sta1@ualberta.ca>
2020-03-12 17:15:10 -0600

    Hamburger menu working with placeholder profile and request fragments

--- app/src/main/java/com/example/suberduberuber/Activities/DashboardActivity.java
+++ app/src/main/java/com/example/suberduberuber/Activities/DashboardActivity.java
@@ -1,16 +1,29 @@
 package com.example.suberduberuber.Activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
+import com.example.suberduberuber.Fragments.profileFragment;
+import com.example.suberduberuber.Fragments.requestFragment;
 import com.example.suberduberuber.R;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -24,6 +37,9 @@ public class DashboardActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
 
     private Toolbar toolbar;
+    private NavController navController;
+    private AppBarConfiguration appBarConfiguration;
+
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
     private NavigationView navigationView;
@@ -37,6 +53,82 @@ public class DashboardActivity extends AppCompatActivity {
 
         currentUser = myAuth.getCurrentUser();
 
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//
+        navigationView = findViewById(R.id.nav_view);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph())
+                .setDrawerLayout(drawerLayout)
+                .build();
+
+        configureToolbar();
+        NavigationUI.setupWithNavController(navigationView, navController);
+        configureNavigationDrawer();
     }
+
+    // for triple dot menu on right
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_menu, navigationView.getMenu());
+        return true;
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_action_menu_white);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //actionBar.setTitle(R.string.app_name);
+    }
+
+    private void configureNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment f = null;
+                int itemId = menuItem.getItemId();
+
+                if (itemId == R.id.profile) {
+                    //f = new ProfileFragment();
+                    f = new profileFragment();
+                }
+                else if (itemId == R.id.requests) {
+                    //f = new RequestFragment();
+                    f = new requestFragment();
+                }
+
+                if (f != null) {
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.nav_host_fragment, f);
+                    transaction.commit();
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return true;
+    }
+
+
 }
 
